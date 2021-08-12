7b8755d8c2a8800c4becfb2bfcb2cff9ce98f4c7
ttuom1nen <ttuominen@outlook.com>
2021-08-12 22:37:37 +0300

    Add log out button

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,20 @@ import CreatePost from "./components/CreatePost/CreatePost";
 import { withAuthenticator } from "aws-amplify-react";
 import "./App.css";
 
+import { Auth } from "aws-amplify";
+
 function App() {
+  async function signOut() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  }
+
   return (
     <div className="App">
+      <button onClick={signOut}>Sign out</button>
       <CreatePost></CreatePost>
       <PostList></PostList>
     </div>
